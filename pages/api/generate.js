cc9ff0cb8e811f2e00aa2e88da6de5f65179d2d4
Syed Arqum Sultan <syedarqum1999@gmail.com>
2023-03-06 13:52:15 +0500

    fixed issuie

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,7 +17,7 @@ const generateAction = async (req, res) => {
 
 
   // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
