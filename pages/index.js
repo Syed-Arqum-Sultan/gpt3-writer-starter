1ec462ff01fe7c6a55987b34576388a89fe9f32d
Syed Arqum Sultan <syedarqum1999@gmail.com>
2023-03-06 12:52:38 +0500

    final changes
    project completed

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,90 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+try{
+    const response = await fetch('http://localhost:3000/api/genrate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+       
+      },
+      body: JSON.stringify({ userInput }),
+    });
+    
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+  
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+    console.log(response.response.data);
+}catch (error){
+  console.log(error)
+}
+  }
+  
+
+
+
+
+
+
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>AI ASSISTANT</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>AI  CAREER  COUNSELLOR</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>GET A CLEAR PICTURE</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          <div className="prompt-buttons">
+             <a className="generate-button" onClick={callGenerateEndpoint}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+          </div>
+
+          {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
       </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+      </div>
+    </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
